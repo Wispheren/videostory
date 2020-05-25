8bb58040134d9e239f6d5a670ccf817e58f50874
Jesper Winther     Web og Apps Aalborg <wjwi@dr.dk>
2020-05-25 06:13:54 +0200

    ...

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './index.css';
-
-let root = document.getElementById('root'),
-addedDiv = document.createElement('div');
-
-addedDiv.appendChild(document.createTextNode('Jubii 3'));
-
-root.appendChild(addedDiv);
